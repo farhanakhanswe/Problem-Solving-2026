c6795f63cbf3bf5c9d35ff6832e5c2e83e6b6140
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2026-01-17 23:39:17 +0600

    Create reverse-linked-list.js

---
+++ LeetCode/JavaScript/Linked List/Easy/reverse-linked-list.js
@@ -0,0 +1,26 @@
+/* Question Link: https://leetcode.com/problems/reverse-linked-list */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null;
+    let curr = head;
+
+    while (curr !== null){
+        const nextTemp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+
+    return prev;
+};
