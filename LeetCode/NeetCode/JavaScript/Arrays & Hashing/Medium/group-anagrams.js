97bf845372212ef63de65602107201c4c03af63e
farhanakhanswe <farhanakhanswe@gmail.com>
2026-01-28 23:52:47 +0600

    js soln for group anagrams

--- LeetCode/NeetCode/JavaScript/Arrays & Hashing/Medium/group-anagrams.js
+++ LeetCode/NeetCode/JavaScript/Arrays & Hashing/Medium/group-anagrams.js
@@ -1,29 +1,36 @@
 /**
+ * Question Link: https://leetcode.com/problems/group-anagrams/
  * @param {string[]} strs
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
     // we are given a bunch of strings and we need to isolate them in subarrays based on who's an anagram to who
-    // if a word does not have an anagram then we will just return only that in a sub array
-    // we will then add the sub array to the array and return it any orer
+    // if a word does not have an anagram then we will just return that word in a sub array
+    // we will then add the sub array to the final array and return it on any order
     // all of them are lowercase english letters in this question so we dont have to add logic to make the words lowercase
 
     // preparing soln:
-    // we can create a map where key is the string and value is the sorted string
-    // we then loop over the map and find the many keys having the same value and store them in a sub array
-    // we then add all the sub arrays to a final array and return that
+    // we can create a map where key is the sorted string and value is the list of anagrams that become this sorted string after sorting
 
-    // incomplete soln:
-    
-    let strMap = new Map()
+    if (strs.length < 1) return [];
+    let sortedStrMap = new Map()
 
-    for (let i = 0; i < strs.length - 1; i++) {
-        if (!strMap.has(strs[i])) {
-            sortedStr = strs[i].sort();
-            strMap.set(strs[i], sortedStr)
+    for (let i = 0; i < strs.length; i++) {
+        let sortedStr = strs[i].split('').sort().join('');
+
+        if (sortedStrMap.has(sortedStr)) {
+            sortedStrMap.get(sortedStr).push(strs[i]);
+        } else {
+            sortedStrMap.set(sortedStr, [strs[i]]);
         }
     }
 
+    let groupOfAnagrams = [];
+
+    for (const value of sortedStrMap.values()) {
+        groupOfAnagrams.push(value)
+    }
+
+
+    return groupOfAnagrams;
 }
-
-
