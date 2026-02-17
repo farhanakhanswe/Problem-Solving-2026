87a39729c38eb4e230e77b6aabe75b590d236bb0
farhanakhanswe <farhanakhanswe@gmail.com>
2026-02-17 11:49:20 +0600

    Add js soln for first element with unique frequency

--- LeetCode Contest Submissions/Weekly Contests/Weekly Contest 489/answers/first-element-with-unique-frequency.js
+++ LeetCode Contest Submissions/Weekly Contests/Weekly Contest 489/answers/first-element-with-unique-frequency.js
@@ -1,55 +1,51 @@
 /* Question Link: https://leetcode.com/problems/first-element-with-unique-frequency/ */
 
-// Incorrect Solution
+// Solution 1: Time Complexity: O(n) & Space Complexity: O(n)
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var firstUniqueFreq = function (nums) {
-    // i am given an integer array nums
-    // i need to return an integer denoting the first element (scanning from left to right) in nums
-    // in numbers whose frequency is unique
+    // i am given an int array nums
+    // i will return an integer denoting the first element (scanning from left to right)
+    // in nums whose frequency is unique
     // that is, no other integer appears the same number of times in nums
     // if there is no such element, return -1
 
-    // so the uniqueness is not the number, it is its frequency
+    // so nums = [20,10,30,30]
+    // here we return 30 as it is the first number with unique freq as we move from
+    // left to right
 
-    // nums = [20, 10, 30, 30]
-    // 20 appears once
-    // 10 appears once
-    // 30 appears twice
-
-    // we can use a map for each number and value would the the frequency
-    // we can then loop for the unique value
-    let numMap = new Map();
+    let numFreqMap = new Map(); // key -> num ; value -> frequency
+    let freqCountMap = new Map(); // key -> frequency; value -> frequency count
 
     for (let i = 0; i < nums.length; i++) {
-        if (numMap.has(nums[i])) {
-            numMap.set(nums[i], numMap.get(nums[i]) + 1);
+        if (numFreqMap.has(nums[i])) {
+            numFreqMap.set(nums[i], numFreqMap.get(nums[i]) + 1);
         } else {
-            numMap.set(nums[i], 1);
+            numFreqMap.set(nums[i], 1);
         }
     }
 
-    // how do we find a unique value
-    // for each key we can check if the value exists as value for any other keys
-
-    for (const [key, value] of numMap) {
-        let isUnique = true;
-        for (const [key1, value1] of numMap) {
-            if (key === key1) continue;
-
-            if (value === value1) {
-                isUnique = false;
-                break;
-            }
+    for (const freq of numFreqMap.values()) {
+        if (freqCountMap.has(freq)) {
+            freqCountMap.set(freq, freqCountMap.get(freq) + 1);
+        } else {
+            freqCountMap.set(freq, 1);
         }
+    }
 
-        if (isUnique) {
-            return key;
+    // now lets try to find the first number with unique frequency as we move from left to right in the array
+
+    for (let i = 0; i < nums.length; i++) {
+        let freq = numFreqMap.get(nums[i]);
+
+        if (freqCountMap.get(freq) === 1) {
+            return nums[i];
         }
     }
 
     return -1;
+
 };
