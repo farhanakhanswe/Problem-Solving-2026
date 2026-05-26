b8cba4c164c2fa690c4afd6280b61ba356c1dff7
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2026-05-26 23:39:11 +0600

    Add notes to add-two-promises.js

--- LeetCode/30 Days of JavaScript/4-Promises and Time/1-add-two-promises.js
+++ LeetCode/30 Days of JavaScript/4-Promises and Time/1-add-two-promises.js
@@ -13,3 +13,25 @@ var addTwoPromises = async function(promise1, promise2) {
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
  *   .then(console.log); // 4
  */
+
+/* 
+    Notes:
+    
+    (1) A Promise is a placeholder for a value that will be available in the future, so we use await to pause
+        execution of the current async function until that value is resolved.
+
+    (2) Here is a more simplified version:
+     
+          var addTwoPromises = async function(promise1, promise2) {
+            const num1 = await promise1;
+            const num2 = await promise2;
+
+            return num1 + num2;
+        };
+
+        Here, we are first waiting to get a value back from promise1 and assign it to num1.
+        Then, we are waiting to get a value back from promise2 and assign it to num2.
+        Finally, we are returning the sum of num1 and num2
+      
+    
+*/
