8625d43ba39f823cbbd383f30c393b5d1fe3d9f0
Farhana <farhanakhanswe@gmail.com>
2026-05-31 13:21:14 +0600

    docs: fixing text spacing in sleep.js notes

--- LeetCode/30 Days of JavaScript/4-Promises and Time/2-sleep.js
+++ LeetCode/30 Days of JavaScript/4-Promises and Time/2-sleep.js
@@ -21,14 +21,20 @@ async function sleep(millis) {
 /*
     Notes:
 
-    (1) The question wants us to return a promise that resolves after
-    millis milliseconds. Every async function returns a promise. When we create a promise object, JS automatically  provides resolve and reject functions to the promise object's callback function.
+    (1) The question wants us to return a promise that resolves after millis milliseconds. Every async function returns a promise. 
+        When we create a promise object, JS automatically  provides resolve and reject functions to the promise object's callback function.
 
         new Promise(function(resolve){})
 
-    A promise object can have 3 states: pending, fulfilled and rejected. The purpose of resolve() is to fulfill the promise, which simply means the promise is completed. 
+        A promise object can have 3 states:
+            (a) pending
+            (b) fulfilled
+            (c) rejected
+            
+        The purpose of resolve() is to fulfill the promise, which simply means the promise is completed. 
 
-    (2) The question says that the promise object can resolve any value, meaning it can return any value when it is fulfilled; so, we don't have to pass any value to the resolve() and leave the resolve() as it is.
+    (2) The question says that the promise object can resolve any value, meaning it can return any value when it is fulfilled; so, we don't have 
+        to pass any value to the resolve() and leave the resolve() as it is.
 
     (3) What we now need to do is call the resolve function of the promise object after millis milliseconds using setTimeout().
 
