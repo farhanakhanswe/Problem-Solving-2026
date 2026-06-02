da8e97a54c12ff6e8d3b4b9328fe68fa78c83faf
Farhana <farhanakhanswe@gmail.com>
2026-06-03 00:24:07 +0600

    docs: add notes for timeout-cancellation.js

--- LeetCode/30 Days of JavaScript/4-Promises and Time/3-timeout-cancellation.js
+++ LeetCode/30 Days of JavaScript/4-Promises and Time/3-timeout-cancellation.js
@@ -42,6 +42,16 @@ var cancellable = function(fn, args, t) {
 /* 
     Notes:
 
-    (1) const cancel = cancellable(log, args, t); Here, cancel is storing the closure 
+    (1) "cancellable()" returns a function called "cancelFn" which is a closure. When we execute the closure, it clears 
+        out the timer for "fn" to stop "fn" from being executed.
+
+    (2) const cancel = cancellable(log, args, t); Here, "cancel" is storing the closure. This closure has access to 
+        the outer variables in the parent function "cancellable", so it does keep track of the timer to execute "fn".
+
+    (3) When we first call the "cancellable()", it starts the delay timer so that "fn" is executed after "t" 
+        milliseconds. However, if we execute the returned closure "cancelFn" before the "t" milliseconds have passed,
+        we have to stop "fn" execution by clearing out the timer using clearTimeout().
+
+    (4) Since "cancel" contains the closure, cancel() needs to be called to clear the timer.
 
 */
