b2e317237b1351790dec7ea50417ee8af1dcfe37
Farhana <farhanakhanswe@gmail.com>
2026-07-12 22:04:56 +0600

    problems: add answer to LC SQL 50 queries-quality-and-percentag question

---
+++ LeetCode/SQL 50/Basic Aggregate Functions/5-queries-quality-and-percentage.sql
@@ -0,0 +1,8 @@
+# Link: https://leetcode.com/problems/queries-quality-and-percentage/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT 
+    query_name,
+    ROUND( AVG(rating/position) , 2) as quality,
+    ROUND( SUM( IF(rating < 3 , 1, 0) ) * 100 / COUNT(*) , 2)  as poor_query_percentage
+FROM Queries
+GROUP BY query_name;
