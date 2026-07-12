7195bcbc78851deeb0cc6bbe0b3d5ea70d730641
Farhana <farhanakhanswe@gmail.com>
2026-07-12 22:22:54 +0600

    problems: add answer to LC SQL 50 monthly-transactions-i question

---
+++ LeetCode/SQL 50/Basic Aggregate Functions/6-monthly-transactions-i.sql
@@ -0,0 +1,10 @@
+# Link: https://leetcode.com/problems/monthly-transactions-i/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT DATE_FORMAT(t.trans_date, '%Y-%m') AS month,
+       country,
+       COUNT(*) AS trans_count,
+       SUM(IF(state = 'approved', 1, 0)) AS approved_count,
+       SUM(amount) AS trans_total_amount,
+       SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+FROM Transactions AS t
+GROUP BY DATE_FORMAT(t.trans_date, '%Y-%m'), country;
